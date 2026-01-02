166fb38235c009305feaba493ed8c4811a9a0771
shl888 <shl888shl888@outlook.com>
2026-01-02 13:26:13 +0800

    Add or update websocket_pool/pools/__init__.py

--- websocket_pool/pools/__init__.py
+++ websocket_pool/pools/__init__.py
@@ -1,8 +1,8 @@
 """
 连接池包 - 包含交易所连接池和全局管理器
 """
-from .exchange_pool import ExchangePool, ExchangePoolConfig
-from .global_pool import GlobalPoolManager, DEFAULT_WORKER_CONFIGS
+from websocket_pool.pools.exchange_pool import ExchangePool, ExchangePoolConfig
+from websocket_pool.pools.global_pool import GlobalPoolManager, DEFAULT_WORKER_CONFIGS
 
 __all__ = [
     'ExchangePool',
