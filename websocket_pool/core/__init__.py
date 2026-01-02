79d1f365273887417c810d669280cb0351900aaf
shl888 <shl888shl888@outlook.com>
2026-01-02 13:26:32 +0800

    Add or update websocket_pool/core/__init__.py

--- websocket_pool/core/__init__.py
+++ websocket_pool/core/__init__.py
@@ -5,11 +5,11 @@ WebSocket连接池核心模块
 __version__ = "1.0.0-stable-simple"
 
 # 暴露核心组件
-from .connection import Connection
-from .worker_base import BaseWorker
-from .data_worker import DataWorker
-from .backup_worker import BackupWorker
-from .monitor import MonitorCenter
+from websocket_pool.core.connection import Connection
+from websocket_pool.core.worker_base import BaseWorker
+from websocket_pool.core.data_worker import DataWorker
+from websocket_pool.core.backup_worker import BackupWorker
+from websocket_pool.core.monitor import MonitorCenter
 
 __all__ = [
     'Connection',
