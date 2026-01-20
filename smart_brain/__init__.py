d91ffb20bad9ff54b60fb2ced2c06540bb8c6777
shl888 <shl888shl888@outlook.com>
2026-01-20 19:30:17 +0800

    Add or update smart_brain/__init__.py

--- smart_brain/__init__.py
+++ smart_brain/__init__.py
@@ -1,8 +1,16 @@
 """
-智能大脑模块
+智能大脑模块 - 重构版
 """
 
-from .core import SmartBrain, start_keep_alive_background
+from .core import SmartBrain
+from .data_manager import DataManager
+from .command_router import CommandRouter
+from .security_manager import SecurityManager
 
-__all__ = ['SmartBrain', 'start_keep_alive_background']
-__version__ = '1.0.0'
+__all__ = [
+    'SmartBrain',
+    'DataManager',
+    'CommandRouter', 
+    'SecurityManager'
+]
+__version__ = '2.0.0'
