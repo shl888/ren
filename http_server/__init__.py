05ba453a1892233e244138818eb4b689f1bb674d
shl888 <shl888shl888@outlook.com>
2026-02-04 13:16:52 +0800

    Add or update http_server/__init__.py

--- http_server/__init__.py
+++ http_server/__init__.py
@@ -3,7 +3,6 @@
 HTTP服务器模块
 """
 from .server import HTTPServer
-# 🚨 删除：from .exchange_api import ExchangeAPI
 from .listen_key_manager import ListenKeyManager
 from .routes.brain import BrainRoutes
 from .service import HTTPModuleService
@@ -11,7 +10,6 @@ from .service import HTTPModuleService
 __version__ = '2.0.0'
 __all__ = [
     'HTTPServer',
-    # 🚨 删除：'ExchangeAPI',
     'ListenKeyManager', 
     'BrainRoutes',
     'HTTPModuleService'
