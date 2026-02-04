fae156c4149c43981376713ba325a1fc3a1fd178
shl888 <shl888shl888@outlook.com>
2026-02-04 13:16:25 +0800

    Add or update private_ws_pool/__init__.py

--- private_ws_pool/__init__.py
+++ private_ws_pool/__init__.py
@@ -2,17 +2,20 @@
 私人WebSocket连接池模块
 """
 from .pool_manager import PrivateWebSocketPool
-from .raw_data_cache import RawDataCache
 from .connection import (
     PrivateWebSocketConnection,
     BinancePrivateConnection,
     OKXPrivateConnection
 )
 
+# 🔴 【修改点】删除RawDataCache导入
+# from .raw_data_cache import RawDataCache  # 删除这行
+
 __version__ = '1.0.0'
 __all__ = [
     'PrivateWebSocketPool',
-    'RawDataCache',
+    # 🔴 【修改点】删除RawDataCache导出
+    # 'RawDataCache',  # 删除这行
     'PrivateWebSocketConnection',
     'BinancePrivateConnection',
     'OKXPrivateConnection'
