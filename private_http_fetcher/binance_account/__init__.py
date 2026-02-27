867025d851093f7bb56252d695508bb8d2c2404a
shl888 <shl888shl888@outlook.com>
2026-02-27 16:53:20 +0800

    Add files via upload

--- private_http_fetcher/binance_account/__init__.py
+++ private_http_fetcher/binance_account/__init__.py
@@ -4,12 +4,7 @@
 """
 
 from .fetcher import PrivateHTTPFetcher
-from .starter import start_account_task
 
 __all__ = [
     'PrivateHTTPFetcher',
-    'start_account_task'
 ]
-
-import logging
-logging.getLogger(__name__).debug("币安账户资产模块已加载")
