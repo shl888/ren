f35c8d9985b957d2356abd673a33515939482ca9
shl888 <shl888shl888@outlook.com>
2026-02-27 15:55:59 +0800

    Add or update http_server/__init__.py

--- http_server/__init__.py
+++ http_server/__init__.py
@@ -3,14 +3,14 @@
 HTTP服务器模块
 """
 from .server import HTTPServer
-from .listen_key_manager import ListenKeyManager
+#from .listen_key_manager import ListenKeyManager
 from .routes.brain import BrainRoutes
 from .service import HTTPModuleService
 
 __version__ = '2.0.0'
 __all__ = [
     'HTTPServer',
-    'ListenKeyManager', 
+#    'ListenKeyManager', 
     'BrainRoutes',
     'HTTPModuleService'
 ]
