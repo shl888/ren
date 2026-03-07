aeda6ca4a030bf89f2edf5f2c21acf9e67c05073
shl888 <shl888shl888@outlook.com>
2026-03-08 04:54:12 +0800

    Add files via upload

--- data_completion_department/__init__.py
+++ data_completion_department/__init__.py
@@ -3,13 +3,11 @@
 接收、存储、提供数据查询接口
 """
 from .receiver import receive_private_data, receive_market_data, get_receiver
-from .data_completion import setup_data_completion_routes
 
 __all__ = [
-    'receive_private_data',    # ← 改这里
-    'receive_market_data',      # ← 改这里
-    'get_receiver',
-    'setup_data_completion_routes'
+    'receive_private_data',
+    'receive_market_data', 
+    'get_receiver'
 ]
 
 __version__ = '1.0.0'
